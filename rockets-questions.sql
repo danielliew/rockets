e64e270bd41dd47c54f6ccb3d8c66141f0aa014d
n111h <noh@groot.localdomain>
2022-04-15 10:40:51 -0400

    added 2 business questions and first pass at queries

--- rockets-questions.sql
+++ rockets-questions.sql
@@ -2,4 +2,19 @@
 This file answers 10 business questions related to the rockets database
 */
 
--- 
+--  which engine type is most common in missions? (JOIN, JOIN, GROUPBY)
+
+FROM        mission
+SELECT      engine.type
+JOIN        rocketEngine    ON      mission.rocketId = rocketEngine.rocketId
+JOIN        engine          ON      rocketEngine.rocketId = engine.rocketId
+GROUP BY    engine.type
+
+--  which company has the rock with the greatest range/height? (JOIN, ORDERBY)
+
+FROM        rocket
+SELECT      rocket.companyName, rocket.rocketName, orbitalReach.height
+JOIN        orbitalReach    ON      rocket.reach = orbitalReach.reach
+ORDER BY    orbitalReach.height
+
+--
