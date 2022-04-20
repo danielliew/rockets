ef612b74f407ed5a4dd47fda1ffcec4bb3c7eaed
Cruse <crusesweeney@gmail.com>
2022-04-20 15:50:37 -0400

    Co-authored-by: danielliew <danielliew@users.noreply.github.com> Co-authored-by: BenjaminMann <megalugia10@users.noreply.github.com>

--- rockets-questions.sql
+++ rockets-questions.sql
@@ -2,27 +2,64 @@
 This file answers 10 business questions related to the rockets database
 */
 
---  Which engine type is most common in missions?
+--  1.  Which engine type is most common in missions?
 
 select count(*) as 'count', engine.type  from mission      
     left join rocketEngine on rocketEngine.rocketId = mission.rocketId     
     left join engine on engine.engineId = rocketEngine.engineId     
     group by engine.type
     order by count desc
-    limit 1
+    limit 1;
 
---  Which company has the rocket with the greatest range/height?
+--  2.  Which company has the rocket with the greatest range/height?
 
 select max(height) as 'maxheight', company.companyName from rocket
 	left join company on company.companyId = rocket.companyId
     group by rocket.companyId
     order by maxheight desc
-    limit 1
+    limit 1;
 
--- List all successful missions that used rockets that have a reach greater than LEO
+-- 3.  List all successful missions that used rockets that have a reach greater than LEO
 
 select * from mission
 	left join rocket r on r.rocketId = mission.rocketId
     left join orbitalReach o on o.reach = r.reach
-    where o.height > (select height from orbitalReach where reach = 'LEO') and mission.outcome = 'Success'
+    where o.height > (select height from orbitalReach where reach = 'LEO') and mission.outcome = 'Success';
     
+-- 4.  What is the average mission success rate?
+
+select round(
+    (
+        (select count(*) from mission where outcome = 'Success') / (select count(*) from mission)
+    ) * 100, 0);
+
+-- 5.  What is the sum of all the payload weight?
+
+select sum(weight) as 'totalWeight', from payload;
+
+-- 6.  How many rockets have "Falcon" in their name?
+
+select * from rocket where rocket.rocketName like "%Falcon%";
+
+-- 7.  Which customers commissioned more than 1 missions?
+
+select count(*) as 'count', customer.customerName from customer
+	left join payload on payload.customerId = customer.customerId
+    group by customer.customerId
+    having count >1;
+
+-- 8.  what is the most common launch location?
+
+select count(*) as 'count', launchLocation.locationName from launchLocation
+	left join mission on mission.locationId = launchLocation.locationId
+    group by launchLocation.locationId
+    order by count desc
+    limit 1
+
+-- 9. 
+
+
+
+
+-- 10.  
+
